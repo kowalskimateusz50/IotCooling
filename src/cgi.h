1b89a82900b3442bc17ffb5cb9f856e575dc4504
kowalskimateusz50 <kowalskimateusz50@gmail.com>
2024-09-14 18:55:45 +0200

    Working template for WebServer

---
+++ src/cgi.h
@@ -0,0 +1,18 @@
+// GPIOs for Leds
+#define LED1	18
+#define LED2	19
+#define LED3	20
+#define LED4	21
+
+/* initialize the CGI handler */
+void  cgi_init(void);
+
+/* CGI handler for LED control */
+const char * cgi_handler_basic(int iIndex, int iNumParams, char *pcParam[], char *pcValue[]);
+/* CGI handler for LED control with feedback*/
+const char * cgi_handler_extended(int iIndex, int iNumParams, char *pcParam[], char *pcValue[]);
+
+
+/* led control and debugging info */
+void Led_On(int led);
+void Led_Off(int led);
--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,99 @@
 #include "FreeRTOS.h"
 #include "task.h"
-#include <stdio.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "lwip/apps/httpd.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+
+#include "lwipopts.h"
+#include "cgi.h"
+#include "ssi.h"
+
 #include "one_wire.h"
 
+//Wifi credentials
+char ssid[] = "MatLan";
+char pass[] = "janrouter3"; 
 
-void led_task(void* dummy)
-{   
-//Debug message
-printf("\nLed task started: ");
+const uint32_t timeout = 1000 * 60 * 10;// 10min
 
-    while (true) {
+void run_server(void *) {
+    
+    httpd_init();
+    ssi_init();
+    cgi_init();
+    printf("Http server initialized.\n");
 
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        vTaskDelay(1000);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        vTaskDelay(1000);
-    }
+    while(true)
+    {
+    };
+
 }
 
 int main()
-{
-    //Initialize cyw_43
+ {
+
+    //Initialize standard peripherials
     stdio_init_all();
-    if (cyw43_arch_init()) {
-        printf("\nWi-Fi init failed");
-        return -1;
+
+    //Initialize wifi chip
+    if (cyw43_arch_init())
+    {
+        printf("\ncyw43 failed to initialise");
+        return 1;
     }
 
-    //Thread handling
-    //xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
-    //vTaskStartScheduler();
+    cyw43_arch_enable_sta_mode();
 
-    One_wire one_wire(22); //GP22 - Pin 29 on Pi Pico
-    one_wire.init();
-    rom_address_t address{};
+    cyw43_wifi_pm(&cyw43_state, cyw43_pm_value(CYW43_NO_POWERSAVE_MODE, 20, 1, 1, 1));
+
+    //Attempt to connect to WiFi network 
+    if (cyw43_arch_wifi_connect_timeout_ms(ssid, pass, CYW43_AUTH_WPA2_AES_PSK, timeout))
+    {
+        printf("\nfailed to connect, resp: ");
+        return 1;
+    }
+    else {
+        //print success message 
+        printf("Connected success\n");
+        extern cyw43_t cyw43_state;
+        auto ip_addr = cyw43_state.netif[CYW43_ITF_STA].ip_addr.addr;
+        printf("IP Address: %lu.%lu.%lu.%lu\n", ip_addr & 0xFF, (ip_addr >> 8) & 0xFF, (ip_addr >> 16) & 0xFF, ip_addr >> 24);
+        // turn on LED to signal connected
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    }
+
+
+    
+
+    //Thread handling
+
+    //Create web server task
+    xTaskCreate(run_server, "run_server", 256, NULL, 1, NULL);
+
+    //Create temperature measurement task
+
+    vTaskStartScheduler();
+    
+    //One_wire one_wire(22); //GP22 - Pin 29 on Pi Pico
+    //one_wire.init();
+    //rom_address_t address{};
 
     printf("\nProgram is starting reading temperature: "); 
 
     while(1){
 
-        one_wire.single_device_read_rom(address);
-        printf("Device Address: %02x%02x%02x%02x%02x%02x%02x%02x\n", address.rom[0], address.rom[1], address.rom[2], address.rom[3], address.rom[4], address.rom[5], address.rom[6], address.rom[7]);
-        one_wire.convert_temperature(address, true, false);
-        printf("Temperature: %3.1foC\n", one_wire.temperature(address));
-        sleep_ms(1000);
+        //one_wire.single_device_read_rom(address);
+        //printf("Device Address: %02x%02x%02x%02x%02x%02x%02x%02x\n", address.rom[0], address.rom[1], address.rom[2], address.rom[3], address.rom[4], address.rom[5], address.rom[6], address.rom[7]);
+        //one_wire.convert_temperature(address, true, false);
+        //printf("Temperature: %3.1foC\n", one_wire.temperature(address));
+        printf("Temperature: is dummy ");
+
         
     };
+    
+
 }
